e5fbbc1c3d66935e06ea5a5a7212b7e8cc3145ef
jang sung ho <tisflyjjang051@gmail.com>
2023-02-24 14:16:14 +0900

    ch06 end

--- date.sql
+++ date.sql
@@ -172,6 +172,40 @@ SELECT EMPNO, ENAME,
     END AS 분기별_입사
 FROM EMP;
 
+---------------------ch06 연습문제 1번 ----------------- 
+
+SELECT  EMPNO , RPAD(SUBSTR(EMPNO,1,2),4,'*') AS MASKING_EMPNO ,
+        ENAME , RPAD(SUBSTR(ENAME,1,1),LENGTH(ENAME),'*') AS MASKING_ENAME 
+FROM EMP
+WHERE LENGTH(ENAME) >=5 AND LENGTH(ENAME) < 6;
+
+
+---------------------ch06 연습문제 2번 ----------------- 
+SELECT EMPNO,ENAME,SAL, 
+TRUNC(SAL/21.5,2) AS DAY_PAY, 
+ROUND(SAL/21.5/8,1) AS TIME_PAY 
+FROM EMP;
+
+
+---------------------ch06 연습문제 3번 ----------------- 
+SELECT EMPNO,ENAME,HIREDATE,  
+TO_CHAR(NEXT_DAY(ADD_MONTHS(HIREDATE,3),'월'),'YYYY-MM-DD') AS R_JOB ,
+NVL(TO_CHAR(COMM),'N/A') AS COMM
+FROM EMP;
+
+---------------------ch06 연습문제 4번 ----------------- 
+SELECT EMPNO,ENAME,MGR,
+    CASE
+        WHEN MGR IS NULL THEN '0000'
+        WHEN SUBSTR(TO_CHAR(MGR),1,2) = '75' THEN '5555'
+        WHEN SUBSTR(TO_CHAR(MGR),1,2) = '76' THEN '6666'
+        WHEN SUBSTR(TO_CHAR(MGR),1,2) = '77' THEN '7777'
+        WHEN SUBSTR(TO_CHAR(MGR),1,2) = '78' THEN '8888'
+        ELSE TO_CHAR(MGR)
+    END AS CHG_MGR
+FROM EMP;
+
+
 
 
 
